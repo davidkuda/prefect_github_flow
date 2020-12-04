1b51f4e53dc26fcc2c83730d64889406cc253058
David Kuda <david.kuda@pm.me>
2020-12-04 23:31:51 +0100

    Create class to request prefect's GraphQL API to register Flows

---
+++ flows/api_client_register_flow.py
@@ -0,0 +1,33 @@
+import requests
+
+
+class RegisterFlow:
+
+    def __init__(self, flow, project_id, base_url):
+        self.flow_bytes = flow.serialize()
+        self.project_id = project_id
+        self.base_url = base_url
+        self.headers = {'Accept-Encoding': 'gzip, deflate, br',
+                        'Content-Type': 'application/json',
+                        'Accept': 'application/json',
+                        'Connection': 'keep-alive',
+                        'DNT': '1',
+                        'Origin': base_url}
+        self.variables = {"flow": self.flow_bytes, "projectId": self.project_id}
+
+    MUTATION_REGISTER_FLOW = '''
+    mutation register_flow($flow: JSON!, $projectId: UUID!) {
+      create_flow(input: { serialized_flow: $flow, project_id: $projectId }) {
+        id
+      }
+    }
+    '''
+
+    def register_flow(self):
+        r = requests.post(
+            self.base_url,
+            json={
+                'query': self.MUTATION_REGISTER_FLOW,
+                'variables': self.variables},
+            headers=self.headers
+        ).json()
