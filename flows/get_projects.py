cf631bd3db15af0a66c61c93af9caedb29b5928f
David Kuda <david.kuda@pm.me>
2020-12-04 11:48:04 +0100

    Update prefect tasks and flow

--- flows/get_projects.py
+++ flows/get_projects.py
@@ -1,10 +1,15 @@
 import requests
-from prefect import task, Flow, config
+from prefect import task, Flow, config, context
 from prefect.environments.storage import GitHub
-from prefect.client.secrets import Secret
+import os
 
 
-BASE_URL = 'https://agent.prefect.docs.pandata.de/'
+# --- Env Vars --- #
+# os.environ['KEY'] = 'VALUE'
+
+
+# BASE_URL = 'https://agent.prefect.docs.pandata.de/'
+BASE_URL = 'http://localhost:4202/'
 query = 'query {project {id name}}'
 headers = {'Accept-Encoding': 'gzip, deflate, br',
            'Content-Type': 'application/json',
@@ -22,32 +27,90 @@ mutation register_flow($flow: JSON!, $projectId: UUID!) {
 '''
 
 
-@task
-def get_data():
-    return [1, 2, 3, 4, 5]
+@task()
+def say_hello():
+    print("Hello, world!")
 
-@task
+@task()
+def get_data():
+    return [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+@task()
+def get_data_bravo():
+    return ['a', 'b', 'c']
+
+@task()
 def print_data(data):
     print(data)
 
-@task
+@task()
+def print_env():
+    print(os.environ)
+
+
+@task()
 def get_projects():
     r = requests.post(BASE_URL, json={'query': query}, headers=headers).json()
     return r
 
 
-with Flow("file-based-flow") as flow:
+# --- Set Schedule // simple --- #
+from datetime import time, timedelta
+from prefect.schedules import IntervalSchedule
+schedule = IntervalSchedule(interval=timedelta(minutes=30))
+
+
+# --- Set Schedule // complex --- #
+# from datetime import time, timedelta
+# from prefect.schedules import Schedule, filters
+# from prefect.schedules.clocks import IntervalClock
+# from prefect.schedules import IntervalSchedule
+#
+#
+# schedule = Schedule(
+#     # emit an event every hour
+#     clocks=[IntervalClock(interval=timedelta(hours=1))],
+#
+#     # only include weekdays
+#     filters=[filters.is_weekday],
+#
+#     # only include 9am and 5pm
+#     or_filters=[
+#         filters.between_times(time(9), time(9)),
+#         filters.between_times(time(17), time(17))
+#     ]
+# )
+#
+schedule.next(4)
+
+
+# --- Register the flow --- #
+
+with Flow(
+        "experiment_flow",
+        schedule=schedule,
+        storage=GitHub(
+            repo="pnd-dkuda/prefect_github_flow",
+            path="flows/get_projects.py",
+            secrets=["GITHUB_ACCESS_TOKEN"]
+        )) as flow:
+    say_hello()
     data = get_data()
+    data_bravo = get_data_bravo()
     print_data(data)
-
-flow.storage = GitHub(
-    repo="pnd-dkuda/prefect_github_flow",
-    path="/blob/main/flows/get_projects.py",
-    secrets=["GITHUB_ACCESS_TOKEN"]
-)
+    print_env()
 
 
-# Register flow to prefect
+# --- Set flow storage to GitHub --- #
+
+# flow.storage = GitHub(
+#     repo="pnd-dkuda/prefect_github_flow",
+#     path="flows/get_projects.py",
+#     secrets=["GITHUB_ACCESS_TOKEN"]
+# )
+
+
+# --- Register flow to prefect --- #
 
 flow_bytes = flow.serialize()
 
@@ -59,6 +122,13 @@ variables = {
 if __name__ == '__main__':
     # r = requests.post(BASE_URL, json={'query': query}, headers=headers).json()
     # print(r)
+
+    if 'HOME' in os.environ:
+        print('HOME environment variable is already defined. Value =', os.environ['HOME'])
+    else:
+        print('HOME environment variable is not defined.')
+
+    print(os.environ)
 
     post = requests.post(BASE_URL, json={'query': mutation_flow, 'variables': variables}, headers=headers).json()
     print(post)
