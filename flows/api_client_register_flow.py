e1e8e63dcc7a25d420912e42a17ae050c69b9158
David Kuda <david.kuda@pm.me>
2020-12-04 23:47:02 +0100

    refactor: Change name of 'base_url' to 'api_base_url'

--- flows/api_client_register_flow.py
+++ flows/api_client_register_flow.py
@@ -3,16 +3,16 @@ import requests
 
 class RegisterFlow:
 
-    def __init__(self, flow, project_id, base_url):
+    def __init__(self, flow, project_id, api_base_url):
         self.flow_bytes = flow.serialize()
         self.project_id = project_id
-        self.base_url = base_url
+        self.base_url = api_base_url
         self.headers = {'Accept-Encoding': 'gzip, deflate, br',
                         'Content-Type': 'application/json',
                         'Accept': 'application/json',
                         'Connection': 'keep-alive',
                         'DNT': '1',
-                        'Origin': base_url}
+                        'Origin': api_base_url}
         self.variables = {"flow": self.flow_bytes, "projectId": self.project_id}
 
     MUTATION_REGISTER_FLOW = '''
